b330f125c305e9965173091fac602f6e54c12e68
Kyle Li <Kyle.Li@lombardrisk.com>
2017-06-09 17:56:42 +0800

    actors scala

---
+++ ActorsScala/src/main/scala/WordCountMultipleCounters/Accumulator.scala
@@ -0,0 +1,32 @@
+/***
+ * Excerpted from "Seven Concurrency Models in Seven Weeks",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material,
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose.
+ * Visit http://www.pragmaticprogrammer.com/titles/pb7con for more book information.
+***/
+package WordCountMultipleCounters
+
+import akka.actor._
+
+import scala.collection.Map
+import scala.collection.mutable.HashMap
+
+case class Counts(counts: Map[String, Int])
+
+class Accumulator extends Actor {
+  
+  val counts = HashMap[String, Int]().withDefaultValue(0)
+
+  def receive = {
+    case Counts(partialCounts) =>
+      for ((word, count) <- partialCounts)
+        counts(word) += count
+  }
+
+  override def postStop() {
+    // for ((k, v) <- counts)
+    //   println(s"$k=$v")
+  }
+}
